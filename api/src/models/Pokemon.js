b007da7827a8b1552360b5d7667c32300d2ceb15
Emerson Villalta <reaperfocs@gmail.com>
2022-07-15 22:50:41 -0500

    model pokemon

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,23 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    hp:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    attack:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
   });
 };
